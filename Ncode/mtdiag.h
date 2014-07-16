ffb3db84d48859f617c410295936d4f67c22487f
Morgan MacLeod <22401914+morganemacleod@users.noreply.github.com>
2014-07-16 14:19:01 -0700

    compilable version

---
+++ Ncode/mtdiag.h
@@ -0,0 +1,16 @@
+c
+c  common block for diagnostic subroutine Nbody4/6
+c  written by Michele Trenti 2005 
+c  this program comes with no warranty
+
+  integer,parameter :: mtNMAX=200000
+  integer,parameter :: mtNMASS=27
+
+  double precision ::  mtx,mtvx,mtm,mttime,mtdr
+  integer ::  mtNTOT,mtNS,mtNB,mtIFIRST,mtN,mtNZERO
+  double precision :: mtMASSdis,mtMASSvalue,mtZMBAR
+       
+  common /mtd/ mtZMBAR,mtNTOT,mtNS,mtNB,mtIFIRST,mtN
+  &    ,mttime,mtdr(3),       
+  &    mtNZERO,mtx(3,mtNMAX), mtvx(3,mtNMAX), mtm(mtNMAX)
+  &    ,mtMASSdis(mtNMASS),mtMASSvalue(mtNMASS)
